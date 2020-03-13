c96d11561ba898f8fff2fab4e216f49fc1934a60
Eric Brewer <eric.r.brewer@gmail.com>
2020-03-13 00:40:34 -0600

    Add keras and imblearn to test script.

--- test.py
+++ test.py
@@ -4,24 +4,34 @@ import time
 
 import tensorflow as tf
 import h5py
+import imblearn
+import keras
 import numpy as np
-import sklearn
 import pandas
+import sklearn
 
-print('sys={}'.format(sys.version))
-if 'SLURM_JOB_ID' in os.environ:
-    stamp = int(os.environ['SLURM_JOB_ID'])
-    print('SLURM_JOB_ID: {:d}'.format(stamp))
-else:
-    stamp = int(time.time())
-    print('Time stamp (s): {:d}'.format(stamp))
-print()
 
-print('h5py={}'.format(h5py.__version__))
-print('numpy={}'.format(np.__version__))
-print('pandas={}'.format(pandas.__version__))
-print('sklearn={}'.format(sklearn.__version__))
-print('tensorflow={}'.format(tf.__version__))
-print('tensorflow.test.is_gpu_available()={}'.format(tf.test.is_gpu_available()))
-print('tensorflow.test.is_gpu_available(cuda_only=True)={}'.format(tf.test.is_gpu_available(cuda_only=True)))
-print('tensorflow.keras={}'.format(tf.keras.__version__))
+def main():
+    print('sys={}'.format(sys.version))
+    if 'SLURM_JOB_ID' in os.environ:
+        stamp = int(os.environ['SLURM_JOB_ID'])
+        print('SLURM_JOB_ID: {:d}'.format(stamp))
+    else:
+        stamp = int(time.time())
+        print('Time stamp (s): {:d}'.format(stamp))
+    print()
+
+    print('h5py={}'.format(h5py.__version__))
+    print('imblearn={}'.format(imblearn.__version__))
+    print('keras={}'.format(keras.__version__))
+    print('numpy={}'.format(np.__version__))
+    print('pandas={}'.format(pandas.__version__))
+    print('sklearn={}'.format(sklearn.__version__))
+    print('tensorflow={}'.format(tf.__version__))
+    print('tensorflow.test.is_gpu_available()={}'.format(tf.test.is_gpu_available()))
+    print('tensorflow.test.is_gpu_available(cuda_only=True)={}'.format(tf.test.is_gpu_available(cuda_only=True)))
+    print('tensorflow.keras={}'.format(tf.keras.__version__))
+
+
+if __name__ == '__main__':
+    main()
